0e1c8856db466dbf3ba3bce744a2982c6b164fb5
Roberta Escher <roberta@totemcoletivo.com.br>
2012-11-06 13:20:44 -0200

    Facebook connect funcionando sem bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,16 +9,32 @@ belongs_to :empresa
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :gender, :name, :location, :image
 
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"]
+        user.gender = data["gender"]
+        user.name = data["name"]
+        user.image = data["access_token.info.image"]
+        user.location = data["location[:name]"]
+      end
+    end
+  end
+
   def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
     data = access_token.extra.raw_info
     if user = User.where(:email => data.email).first
       user
     else # Create a user with a stub password.
-      u = User.create!(:email => data.email, :password => Devise.friendly_token[0,20], :name => data.name,:gender => data.gender, :location => data.location[:name], :image => data.pic_big)
+      u = User.create!(:email => data.email, :password => Devise.friendly_token[0,20], :name => data.name,:gender => data.gender, :location => data.location[:name], :image => access_token.info.image)
       # This is needed becase facebook_id is a protected attribute
       u.facebook_id = data.id
       u.save
+      user
     end
   end
+
+
+
 end
 
