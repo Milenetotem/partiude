cb0eb736a628098c24c83e42ce8ed5b34282013d
Enzo Z <enzozuccolotto@gmail.com>
2012-12-19 10:05:15 -0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.9'
+
+gem 'pg'
+# gem 'paperclip'
+# gem 'validates_timeliness'
+gem 'jquery-rails'
+
+group :test do
+  gem 'rspec'
+  gem 'sqlite3'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Partiude::Application.config.secret_token = 'f3641c075818b49c3fbb20517c802b4a157104e074b391debd943742d231dd67cf2c06678fab8f3eb03e0f4d7724683f33fd2dfab0481f237fe5d90665375cc9'
