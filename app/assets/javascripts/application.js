5eac1909b7bb988d7aa5189c548c70ad4cf7ac40
Enzo Z <enzozuccolotto@gmail.com>
2013-01-04 13:01:10 -0200

    opsie

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
+//= require form
 //= require_tree .
--- app/helpers/route_form_builder.rb
+++ app/helpers/route_form_builder.rb
@@ -4,11 +4,6 @@ class RouteFormBuilder < ActionView::Helpers::FormBuilder
   include ActionView::Helpers::FormTagHelper
 
   def transport_type_field
-    #   <dl class="sub-nav">
-    #     <dd><a href="#" class="carro">&nbsp;</a></dd>
-    #     <dd><a href="#" class="ape">&nbsp;</a></dd>
-    #     <dd><a href="#" class="bicicleta">&nbsp;</a></dd>
-    #   </dl>
     transport_field_content =  label :transport_type
     transport_field_content += radio_button(:transport_type, "to_be_replaced")
     car_link = link_to "", "#", :class => 'carro'
