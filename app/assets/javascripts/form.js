67701e431a3e702ad6d58149027e860bb9815818
Enzo Z <enzozuccolotto@gmail.com>
2013-01-04 18:27:18 -0200

    database?

--- app/assets/javascripts/form.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-  $(".radio-selector input:radio").addClass("input_hidden");
-  $(".radio-selector label").click(function(){
-      $(this).addClass('selected').siblings().removeClass('selected');
-  });
-  $(".radio-selector span.radio").remove();
-});
