5eba4e5aabfe4d7ee65c5fabfa7fdaba16c137b1
Roberta Escher <roberta@totemcoletivo.com.br>
2013-01-05 14:50:27 -0200

    Translation scss

--- app/helpers/route_form_builder.rb
+++ app/helpers/route_form_builder.rb
@@ -7,9 +7,9 @@ class RouteFormBuilder < ActionView::Helpers::FormBuilder
     transport_field_content =  label :transport_type
     transport_field_content += hidden_field(:transport_type)
 
-    tags = content_tag("dd", tag("span", :class=>"carro", :data=>{:value => "to_be_replaced"}))
-    tags += content_tag("dd", tag("span", :class=>"bicicleta", :data=>{:value => "to_be_replaced"}))
-    tags += content_tag("dd", tag("span", :class=>"ape", :data=>{:value => "to_be_replaced"}))
+    tags = content_tag("dd", tag("span", :class=>"car", :data=>{:value => "to_be_replaced"}))
+    tags += content_tag("dd", tag("span", :class=>"bike", :data=>{:value => "to_be_replaced"}))
+    tags += content_tag("dd", tag("span", :class=>"walk", :data=>{:value => "to_be_replaced"}))
     transport_field_content += content_tag("dl", tags, :class=> "sub-nav")
 
     content_tag(:div, transport_field_content, :class => "six columns radio-selector", :data => {:target => "route_transport_type"})
