38a3c525148c99dd6559e5f0bd29295cd0cca8c9
Roberta Escher <roberta@totemcoletivo.com.br>
2013-01-05 15:44:22 -0200

    New form

--- app/helpers/route_form_builder.rb
+++ app/helpers/route_form_builder.rb
@@ -10,13 +10,13 @@ class RouteFormBuilder < ActionView::Helpers::FormBuilder
     tags = content_tag("dd", tag("span", :class=>"car", :data=>{:value => "to_be_replaced"}))
     tags += content_tag("dd", tag("span", :class=>"bike", :data=>{:value => "to_be_replaced"}))
     tags += content_tag("dd", tag("span", :class=>"walk", :data=>{:value => "to_be_replaced"}))
-    transport_field_content += content_tag("dl", tags, :class=> "sub-nav")
+    transport_field_content += content_tag("dl", tags, :class=> "sub-nav transport")
 
-    content_tag(:div, transport_field_content, :class => "six columns radio-selector", :data => {:target => "route_transport_type"})
+    content_tag(:div, transport_field_content, :class => "twelve columns radio-selector", :data => {:target => "route_transport_type"})
   end
 
   def repeat_in_field
     repeat_in_content = select(:repeat_in, RepeatIn.to_options, {}, {:class => "expand"})
-    content_tag(:div, repeat_in_content, :class => "four columns")
+    content_tag(:div, repeat_in_content, :class => "six columns")
   end
 end
