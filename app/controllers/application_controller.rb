a952ad3d424571ff69e2312b2160f529fe394f8e
Enzo Z <enzozuccolotto@gmail.com>
2013-01-25 20:04:56 -0200

    yey, recurring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,14 @@ class ApplicationController < ActionController::Base
 
   respond_to :html, :json, :xml, :js, :text, :pdf
 
+
+private
+  def after_sign_in_path_for(resource)
+    itineraries_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
 end
