6d4a194a3c86bf3932c6254215f1ad1e39063765
Enzo Z <enzozuccolotto@gmail.com>
2013-01-30 17:36:59 -0200

    recurring

--- app/models/recurring.rb
+++ app/models/recurring.rb
@@ -1,3 +1,5 @@
+require 'recurrence'
+
 class Recurring < ActiveRecord::Base
   attr_accessible :repeat_weekly_interval, :repeat_in, :hour, :begin_day, :itinerary,
                   :sunday, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday
@@ -9,21 +11,43 @@ class Recurring < ActiveRecord::Base
   validates_presence_of :repeat_in
   validates_presence_of :hour
   validates_presence_of :begin_day
-  validate :check_weekly_selected_days
+  validate :check_weekly_selected_days, :check_monthly_selected_days
 
   def next_occur
-    nil
+    return hour if RepeatIn::TODAY
+
+    case repeat_in
+    when RepeatIn::DAILY
+      options = {:every => :day, :start => start, :interval => 24}
+    when RepeatIn::WEEKLY
+      options = {:every => :week, :start => start, :on => repeation_days}
+    when RepeatIn::MONTHLY
+      options = {:every => :month, :start => start, :on => repeation_days}
+    end
+    Recurrence.new(options).next
   end
 
 private
   def check_weekly_selected_days
-    if repeat_in_weekly? && !selected_any_day?
+    if repeat_in_weekly? && repeation_days.size < 1
       errors.add(:repeat_in, :select_at_least_one_day)
     end
   end
 
-  def selected_any_day?
-    sunday || monday || tuesday || wednesday || thursday || friday || saturday
+  def check_monthly_selected_days
+    if repeat_in_monthly? && repeation_days.size != 1
+      errors.add(:repeat_in, :select_only_one_day)
+    end
+  end
+
+  def start
+    DateTime.parse("#{begin_day.to_date} #{hour}")
+  end
+
+  def repeation_days
+    DaysOfWeek.list.select do |day|
+      day.to_sym if send day.to_sym
+    end
   end
 
 end
--- spec/models/recurring_spec.rb
+++ spec/models/recurring_spec.rb
@@ -40,6 +40,34 @@ describe Recurring do
       end
     end
 
+    context "repeat in month" do
+      let! (:options) {
+        {:repeat_in => RepeatIn::MONTHLY, :hour => "00:00", :begin_day => Date.today, :itinerary => Itinerary.new}
+      }
+
+      it "verify if there is one day selected" do
+        recurring = Recurring.new(options)
+        recurring.valid?
+        recurring.errors.keys.should include(:repeat_in)
+      end
+
+      it "verify if there is more than one day selected" do
+        recurring = Recurring.new(options)
+        recurring.sunday = recurring.monday = true
+        recurring.valid?
+        recurring.should have(1).error_on(:repeat_in)
+      end
+
+      it "save when one day is selected" do
+        recurring = Recurring.new(options)
+        recurring.sunday = true
+        expect{
+          recurring.save!
+        }.to change(Recurring, :count).by(1)
+      end
+
+    end
+
     it "save when everything is ok" do
       options = {:repeat_in => RepeatIn::TODAY, :hour => "00:00",
                  :begin_day => Date.today, :itinerary => Itinerary.new}
@@ -53,7 +81,13 @@ describe Recurring do
   end
 
   context "#next_occur" do
+    let(:today) {}
+    let(:daily) {}
+    let(:weekly) {}
+    let(:monthly) {}
+
     it { should respond_to(:next_occur) }
+
   end
 
 end
