08ab92198325ca3c2f7fbb2e3b69cb62b6224cc4
Roberta Escher <roberta@totemcoletivo.com.br>
2013-02-05 00:09:49 -0200

    New layout to My Itineraries

--- app/helpers/recurring_form_builder.rb
+++ app/helpers/recurring_form_builder.rb
@@ -5,7 +5,7 @@ class RecurringFormBuilder < ActionView::Helpers::FormBuilder
 
   def repeat_in_field
     repeat_in_content = select(:repeat_in, RepeatIn.to_options.reverse, {}, {:class => "expand"})
-    content_tag(:div, repeat_in_content, :class => "six")
+    content_tag(:div, repeat_in_content, :class => "six end")
   end
 
   def day_of_week_check_box(day)
