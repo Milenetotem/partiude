572a494adc21e9b507b6c1f12fae62eb33446377
Enzo Z <enzozuccolotto@gmail.com>
2013-02-05 00:52:54 -0200

    misc

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130201150737) do
+ActiveRecord::Schema.define(:version => 20130205014018) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
@@ -35,8 +35,9 @@ ActiveRecord::Schema.define(:version => 20130201150737) do
   create_table "participants", :force => true do |t|
     t.integer  "user_id"
     t.integer  "itinerary_id"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
+    t.boolean  "driver",       :default => false
   end
 
   create_table "points", :force => true do |t|
@@ -64,6 +65,16 @@ ActiveRecord::Schema.define(:version => 20130201150737) do
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "",     :null => false
